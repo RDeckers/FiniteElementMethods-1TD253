424f9dd39c6532f111ca00691b45d4e61842bae1
R. Deckers <github.com@codingcat.nl>
2015-11-19 18:37:51 +0100

    siwtched ** to ^

--- assignment_01/code/my_first_fem_solver.m
+++ assignment_01/code/my_first_fem_solver.m
@@ -1,9 +1,35 @@
 function my_first_fem_solver(N)
-a = -1; % left end point of interval
+a = 0; % left end point of interval
 b = 1; % right
 h = (b-a)/N; % mesh size
 x = a:h:b; % node coords
-A=my_stiffness_matrix_assembler(x);
-B=my_load_vector_assembler(x);
-xi = A\B; % solve system of equations
-%plot(x,xi) % plot solution
+low =  0;
+high = 0.;
+B=my_load_vector_assembler(x, low, high, @f);
+M = mass_matrix(x); %last points kept fixed
+%M(1,:) = [];
+%M(end,:) = [];
+A_fixed =stiffness_matrix_fixed(x,low, high);
+xi_fixed = A_fixed\B; % solve system of equations
+reversed = (-A_fixed*xi_fixed);
+lap_fixed = M \(reversed(2:end-1));
+figure(1)
+plot(x,xi_fixed) % plot solution
+figure(2)
+hold on;
+F = arrayfun(@f, x(2:end-1)).';
+delta = abs(F+lap_fixed);
+plot(x(2:end-1), delta)
+x
+rho = trapezoidal(x, abs(F+lap_fixed));
+figure(3)
+hold on;
+plot(rho)
+%figure(3)
+%plot(x, lap_fixed-lap_huh)
+
+function y=f(x)
+  %y=2;
+  %y=exp(-1000*(x-0.5)^2);
+  %y = pi^2*49*sin(x*pi*7);
+  y = x*(x-1)
